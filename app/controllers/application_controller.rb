c8b439912bdd130aab6e0cfcecdaa2dcc5a7d82a
Tsubasa-Kasuga <tsubasa.kasuga20@gmail.com>
2020-09-05 22:04:13 +0900

    ユーザー項目追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
-    def hello
-        render html: "hello, world!"
-    end
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+  def configure_permitted_parameters
+    # サインアップ時にnameのストロングパラメータを追加
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :birth_date, :sex])
+    # アカウント編集の時にnameのストロングパラメータを追加
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
+  validates :birth_date, presence: true
+  validates :sex, presence: true
+  enum sex: { man: 0, woman: 1}
 end
