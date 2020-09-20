b55bedf9b25e88fe69a4cc6d0b2f44342ab5b805
Tsubasa-Kasuga <tsubasa.kasuga20@gmail.com>
2020-09-20 11:59:04 +0900

    管理者のdeviseレイアウト変更

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -3,4 +3,9 @@ class Teacher < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # バリデーション
+  validates :name, presence: true
+  validates :birth_date, presence: true
+  validates :sex, presence: true
+  enum sex: { man: 0, woman: 1}
 end
