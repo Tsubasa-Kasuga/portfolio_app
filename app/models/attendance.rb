3ad62dd7d4763510032f59f95520354049c8d0a2
Tsubasa-Kasuga <tsubasa.kasuga20@gmail.com>
2020-10-01 19:01:10 +0900

    受講テーブル作成

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,2 +1,5 @@
 class Attendance < ApplicationRecord
+  # 関連付け
+  belongs_to :teacher
+  belongs_to :teacher
 end
--- db/migrate/20201001080629_create_attendances.rb
+++ db/migrate/20201001080629_create_attendances.rb
@@ -1,12 +1,13 @@
 class CreateAttendances < ActiveRecord::Migration[6.0]
   def change
     create_table :attendances do |t|
-      t.integer :user_id
-      t.integer :lesson_id
-      t.boolean :termination
-      t.boolean :deleted
+      t.integer :user_id, :null => false
+      t.integer :lesson_id, :null => false
+      t.boolean :termination, :null => false, :default => false
+      t.boolean :deleted, :null => false, :default => false
 
       t.timestamps
     end
+    
   end
 end
