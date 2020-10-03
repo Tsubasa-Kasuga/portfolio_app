eb6341a8d51616c073d38f87bd4d1f44ed2fcc65
Tsubasa-Kasuga <tsubasa.kasuga20@gmail.com>
2020-10-03 12:34:46 +0900

    ユーザー側マイページのレッスン情報取得SQL追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
     # 会員情報取得
     @user = User.find(params[:id])
     # 申請中のレッスンを取得
-    # @lessons_attendanced = @user.lessons
+    # SELECT `lessons`.* FROM `lessons` INNER JOIN `attendances` ON `attendances`.`lesson_id` = `lessons`.`id` LIMIT 11
+    @lessons_attendanced = Teacher.joins(lessons: :attendance).select('lessons.*, teachers.name').where(attendances: {user_id: 4})
     # 終了しているレッスンを取得
     @lessons_finished = Lesson.where(teacher_id: params[:id], deleted: true)
   end
