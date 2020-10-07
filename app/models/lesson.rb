75c9f239d6d16df96c8fd21ff3aa8d2d24b1514e
Tsubasa-Kasuga <tsubasa.kasuga20@gmail.com>
2020-10-07 16:55:59 +0900

    レッスン登録ページのレイアウト修正

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,4 +3,10 @@ class Lesson < ApplicationRecord
   belongs_to :teacher
   has_many :attendance
   mount_uploader :lessonImage, AvatarUploader
+
+  enum frequency:{
+    ワンポイントレッスン: 0,
+    月4回コース: 1,
+    月2回コース: 2
+  }
 end
