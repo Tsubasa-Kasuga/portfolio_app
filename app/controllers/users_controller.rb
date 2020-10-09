f403db9d95e475a5ee1cece6c554f0d7832a2d2e
Tsubasa-Kasuga <tsubasa.kasuga20@gmail.com>
2020-10-09 14:21:19 +0900

    ユーザーマイページ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     # 申請中のレッスンを取得
     # SELECT `lessons`.* FROM `lessons` INNER JOIN `attendances` ON `attendances`.`lesson_id` = `lessons`.`id` LIMIT 11
-    @lessons_attendanced = Teacher.joins(lessons: :attendance).select('lessons.*, teachers.name').where(attendances: {user_id: params[:id]})
+    @lessons_attendanced = Teacher.joins(lessons: :attendance).select('lessons.*').where(attendances: {user_id: params[:id]}).paginate(page: params[:page], per_page: 3)
+    # @lessons_attendanced = Lesson.joins(:teacher).select('lessons.*').find(params[:id])
+    @lessons_attendanced = Lesson.joins(:teacher, :attendance).select('lessons.*').where(attendances: {user_id: params[:id]}).paginate(page: params[:page], per_page: 3)
     # 終了しているレッスンを取得
-    @lessons_finished = Lesson.where(teacher_id: params[:id], deleted: true)
+    @lessons_finished = Lesson.where(teacher_id: params[:id], deleted: true).paginate(page: params[:page], per_page: 3)
   end
 end
