cd5cb698146f64ce4987769765d725d4768ea039
Tsubasa-Kasuga <tsubasa.kasuga20@gmail.com>
2020-10-09 21:42:53 +0900

    レッスン編集機能追加

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -19,6 +19,20 @@ class LessonsController < ApplicationController
     redirect_to @lesson
   end
 
+  def edit
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def update
+    @lesson = Lesson.find(params[:id])
+  
+    if @lesson.update(lesson_params)
+      redirect_to @lesson
+    else
+      render 'edit'
+    end
+  end
+
   private
     def lesson_params
         params.require(:lesson).permit(:title, :description, :time, :price, :frequency, :lessonImage)
