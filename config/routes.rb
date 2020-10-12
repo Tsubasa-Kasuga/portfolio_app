d5d6ab6efd6ee9d24f5aca8e1fd21b27b3671084
Tsubasa-Kasuga <tsubasa.kasuga20@gmail.com>
2020-10-12 12:30:06 +0900

    管理者モデルdevise導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  
   get 'attendances/new'
   get 'teachers/show'
   root 'home#index'
@@ -7,11 +8,14 @@ Rails.application.routes.draw do
   resources :attendances, only: [:create, :destroy]
 
   # 認証に必要なルーティングを自動で設定
+  devise_for :admins
   devise_for :teachers
   devise_for :users
-
+  
 
   resources :users, only: [:show]
   resources :teachers, only: [:show]
+  resources :admins, only: [:index], path: '/admins'
+  resources :users, path: '/admins/users'
 
 end
