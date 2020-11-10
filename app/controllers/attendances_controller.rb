6bb53eb58e541d21093f5d1572075f3f1fcdcdfd
Tsubasa-Kasuga <tsubasa.kasuga20@gmail.com>
2020-11-10 15:58:07 +0900

    レッスン詳細画面の非ログイン時のメッセージを修正

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,7 +2,7 @@ class AttendancesController < ApplicationController
   def create
     @lesson = Lesson.find(params[:lesson_id])
     Attendance.create(user_id: current_user.id, lesson_id: params[:lesson_id])
-    redirect_to @lesson, notice: "申し込みが完了しました"
+    redirect_to @lesson, notice: "申し込みが完了しました。講師からのメールをお待ちください。"
   end
 
   def destroy
